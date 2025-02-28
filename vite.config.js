9af10b30f14d9f7fb7cd009466bf7338957258d8
semajssor <141041402+semajssor@users.noreply.github.com>
2025-02-28 10:30:17 +0100

    Fix package.json and Vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Fix for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/face_recognition_brain/"
-})
+  base: "/face_recognition_brain/", // Change this to your repository name
+  build: {
+    outDir: "dist"
+  }
+});
