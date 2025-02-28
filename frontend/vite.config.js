a9fab69d12dab86e6d19dafc0748506f4193e0be
semajssor <141041402+semajssor@users.noreply.github.com>
2025-02-28 15:04:21 +0100

    Deployment on Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Fix for GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/face_recognition_brain/", // Change this to your repository name
   build: {
-    outDir: "dist"
-  }
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html"
+    },
+  },
 });
